a69dd9d0c3d40a753649952c75b4c27e79b06877
Kamron <kamronpoosti@yahoo.com>
2022-12-07 11:10:05 -0800

    Feature 15 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("", redirect_to_home, name="home"),
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
-    path("accounts/", include("accounts.urls"))
+    path("accounts/", include("accounts.urls")),
+    path("tasks/", include("tasks.urls"))
 
 ]
